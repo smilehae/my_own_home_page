bee7151231be873b6667fe0547e826f1cef7fd37
smilehae <mihaepyun@gmail.com>
2022-03-03 01:52:56 +0900

    시계 구현 완료

--- app.js
+++ app.js
@@ -1,23 +1,59 @@
-const title = document.querySelector(".helloTitle:nth-child(2)");
-title.addEventListener("click", () => {
-  title.style.color = "red";
-  title.textContent = "touched!";
-});
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
+const clock = document.querySelector("#clock");
+const HIDDEN_CLASSNAME = "hidden";
 
-title.addEventListener("mouseenter", () => {
-  console.log("mouse entered!");
-});
-console.log(title);
+let username = localStorage.getItem("username") || "";
+let greetingString = "Good Morning";
 
-window.addEventListener("resize", () => {
-  console.log("resized!");
-});
-window.addEventListener("copy", () => {
-  console.log("copyed!");
-});
-window.addEventListener("paste", () => {
-  console.log("pasted!");
-});
-window.addEventListener("offline", () => {
-  alert("there's no wifi!");
+if (username) {
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = greetingString + ", " + username + ".";
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const makeTimewithZero = (time) => {
+  if (+time < 10) {
+    return `0${time}`;
+  }
+  return `${time}`;
+};
+
+setInterval(() => {
+  const today = new Date();
+  const ampmString = document.querySelector(".ampm");
+  let hour = today.getHours();
+  let timeString = "";
+
+  if (hour >= 12 && hour < 17) {
+    greetingString = "Good Afternoon";
+  } else if (hour < 21) {
+    greetingString = "Good Evening";
+  } else if (hour < 24) {
+    greeting = "Good Night";
+  }
+  if (hour > 12) {
+    hour = hour - 12;
+    ampmString.textContent = "PM";
+  } else {
+    ampmString.textContent = "AM";
+  }
+
+  timeString += ` ${makeTimewithZero(hour)}:${makeTimewithZero(
+    today.getMinutes()
+  )}`;
+
+  clock.textContent = timeString;
+}, 1000);
+
+//eventListner에서 어떤 함수를 실행하든 간에, event에 대한 정보를 전달한다. preventDefault는 default인 행동을 막는 것이다.
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem("username", username);
 });
