0bc0811ff16adcb50948f1702ea875b4202980b3
smilehae <mihaepyun@gmail.com>
2022-03-03 00:42:22 +0900

    querySelector은 css처럼 접근하므로 :nth-child(2)등도 가능하다

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-const calculator = {
-  add: (a, b) => a + b,
-  sub: (a, b) => a - b,
-  mul: (a, b) => a * b,
-  div: (a, b) => a / b,
-  pow: (a, b) => Math.pow(a, b),
-};
-
-const addResult = calculator.add(5, 7);
-const subResult = calculator.sub(5, 7);
-const mulResult = calculator.mul(5, 7);
-const powResult = calculator.pow(5, 7);
-const divResult = calculator.div(5, 7);
-
-console.log(addResult, subResult, mulResult, powResult, divResult);
+const title = document.querySelector(".helloTitle:nth-child(2)");
+console.log(title);
