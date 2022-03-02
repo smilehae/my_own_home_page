9d08b08f444b981f030c5380fe68631af00fd5a6
smilehae <mihaepyun@gmail.com>
2022-03-03 00:32:48 +0900

    객체 만들기 복습

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("hi");
+const calculator = {
+  add: (a, b) => a + b,
+  sub: (a, b) => a - b,
+  mul: (a, b) => a * b,
+  div: (a, b) => a / b,
+  pow: (a, b) => Math.pow(a, b),
+};
+
+const addResult = calculator.add(5, 7);
+const subResult = calculator.sub(5, 7);
+const mulResult = calculator.mul(5, 7);
+const powResult = calculator.pow(5, 7);
+const divResult = calculator.div(5, 7);
+
+console.log(addResult, subResult, mulResult, powResult, divResult);
