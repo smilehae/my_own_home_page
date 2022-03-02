645e92163a0593f6270a9ffd328edf65445a12b0
smilehae <mihaepyun@gmail.com>
2022-03-03 00:52:08 +0900

    window와 관련된, resize, copy, paste, offline 등의 event가 존재한다

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 const title = document.querySelector(".helloTitle:nth-child(2)");
+title.addEventListener("click", () => {
+  title.style.color = "red";
+  title.textContent = "touched!";
+});
+
+title.addEventListener("mouseenter", () => {
+  console.log("mouse entered!");
+});
 console.log(title);
+
+window.addEventListener("resize", () => {
+  console.log("resized!");
+});
+window.addEventListener("copy", () => {
+  console.log("copyed!");
+});
+window.addEventListener("paste", () => {
+  console.log("pasted!");
+});
+window.addEventListener("offline", () => {
+  alert("there's no wifi!");
+});
