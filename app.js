eeb5ee09c1bdf002cc1471131df8678d9d33b80f
smilehae <mihaepyun@gmail.com>
2022-03-03 22:18:55 +0900

    padStart를 이용해 앞에 0 간단히 추가할 수 있다

--- app.js
+++ app.js
@@ -10,54 +10,25 @@ let greetingString = "";
 
 if (username) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  greeting.innerText = greetingString + ", " + username + ".";
+  greeting.innerText = "Hello, " + username + ".";
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const makeTimewithZero = (time) => {
-  if (+time < 10) {
-    return `0${time}`;
-  }
-  return `${time}`;
-};
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.textContent = `${hours}:${minutes}:${seconds}`;
+}
 
-setInterval(() => {
-  const today = new Date();
-  const ampmString = document.querySelector(".ampm");
-  let hour = today.getHours();
-  let timeString = "";
-
-  if (hour < 12) {
-    greetingString = "Good Morning";
-  } else if (hour < 17) {
-    greetingString = "Good Afternoon";
-  } else if (hour < 21) {
-    greetingString = "Good Evening";
-  } else if (hour < 24) {
-    greetingString = "Good Night";
-  }
-
-  if (hour > 12) {
-    hour = hour - 12;
-    ampmString.textContent = "PM";
-  } else {
-    ampmString.textContent = "AM";
-  }
-
-  timeString += ` ${makeTimewithZero(hour)}:${makeTimewithZero(
-    today.getMinutes()
-  )}`;
-
-  greeting.textContent = `${greetingString}, ${username}.`;
-  clock.textContent = timeString;
-}, 1000);
+setInterval(getClock, 1000);
 
 //eventListner에서 어떤 함수를 실행하든 간에, event에 대한 정보를 전달한다. preventDefault는 default인 행동을 막는 것이다.
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  greeting.innerText = "Hello " + username;
   greeting.classList.remove(HIDDEN_CLASSNAME);
   localStorage.setItem("username", username);
 });
