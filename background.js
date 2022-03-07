aa8fb5539235f053e3621c9b7bf349141682a685
smilehae <mihaepyun@gmail.com>
2022-03-07 13:51:31 +0900

    Feature : Unsplash API 이용해 랜덤 사진 기능 추가

--- background.js
+++ background.js
@@ -1,8 +1,21 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
+const splashApi = unsplashAPI;
 
 const todayImage = images[Math.floor(Math.random() * images.length)];
 
-const backgroundImg = document.createElement("img");
-backgroundImg.src = `/img/${todayImage}`;
+const backgroundImg = document.createElement("div");
+backgroundImg.style.background = `url(./img/1.jpg) center`;
 backgroundImg.classList.add("back-img");
 document.body.appendChild(backgroundImg);
+
+fetch("https://api.unsplash.com/photos/random?topic=wallpepers", {
+  method: "GET",
+  headers: {
+    Authorization: `Client-ID ${splashApi}`,
+  },
+})
+  .then((response) => response.json())
+  .then(
+    (myJson) =>
+      (backgroundImg.style.background = `url(${myJson.urls.full}) center`)
+  );
