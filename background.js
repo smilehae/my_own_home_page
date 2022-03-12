40bcfd40201018a17dea81ebcf077039b91113cf
smilehae <mihaepyun@gmail.com>
2022-03-13 01:12:52 +0900

    배경 화면 추가, 배경 화면과 함께 작가 이름과 링크 추가

--- background.js
+++ background.js
@@ -1,11 +1,38 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
 // const splashApi = unsplashAPI;
-
+const images = [
+  {
+    imageUrl: "relax_onojeghuo.jpg",
+    photographer: "Clem Onojeghuo",
+    link: "https://unsplash.com/@clemono?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+  },
+  {
+    imageUrl: "mountain_daniel_leone.jpg",
+    photographer: "Daniel Leone",
+    link: "https://unsplash.com/@danielleone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+  },
+  {
+    imageUrl: "books_alfons-morales.jpg",
+    photographer: "Alfons Morales",
+    link: "https://unsplash.com/@alfonsmc10",
+  },
+  {
+    imageUrl: "sea_sean-oulashin.jpg",
+    photographer: "Sean Oulashin",
+    link: "https://unsplash.com/@oulashin?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+  },
+  {
+    imageUrl: "pink_sea_Tyler_Lastovich.jpg",
+    photographer: "Tyler Lastovich",
+    link: "https://unsplash.com/@lastly",
+  },
+];
+const photoText = document.querySelector(".photographer");
 const todayImage = images[Math.floor(Math.random() * images.length)];
 
 const backgroundImg = document.createElement("div");
-backgroundImg.style.background = `url(./img/1.jpg) center`;
+backgroundImg.style.background = `url(./img/${todayImage.imageUrl}) center`;
 backgroundImg.classList.add("back-img");
+photoText.innerHTML = `<a target="blank" href="${todayImage.link}">${todayImage.photographer}</a>`;
 document.body.appendChild(backgroundImg);
 
 // fetch("https://api.unsplash.com/photos/random?topic=wallpepers", {
